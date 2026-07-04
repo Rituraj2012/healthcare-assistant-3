6280baebfc29c46ee5ffa2cf97bff674665b1fa9
Rituraj2012 <asusvivobook16replit@gmail.com>
2026-07-04 11:49:06 +0530

    Refactor checkBP function to calculateBMI for BMI

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-function checkBP() {
+function calculateBMI() {
 
-  let sys = Number(document.getElementById("sys").value);
-  let dia = Number(document.getElementById("dia").value);
+  let weight = Number(document.getElementById("weight").value);
+  let height = Number(document.getElementById("height").value) / 100;
+
+  let bmi = weight / (height * height);
 
   let result = "";
 
-  if (sys >= 140 || dia >= 90) {
-    result = "High Blood Pressure";
-  }
-  else if (sys < 90) {
-    result = "Low Blood Pressure";
-  }
-  else {
-    result = "Normal Blood Pressure";
+  if (bmi < 18.5) {
+    result = "Underweight";
+  } else if (bmi < 25) {
+    result = "Normal Weight";
+  } else if (bmi < 30) {
+    result = "Overweight";
+  } else {
+    result = "Obese";
   }
 
-  document.getElementById("result").innerText = result;
+  document.getElementById("bmiResult").innerText =
+    "BMI: " + bmi.toFixed(1) + " (" + result + ")";
 }
