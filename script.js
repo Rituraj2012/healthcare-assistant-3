9613c6064bcae62fd42fbd0f1f7e0116d01896d2
Rituraj2012 <asusvivobook16replit@gmail.com>
2026-07-04 12:03:08 +0530

    Implement blood pressure check function
    Added a function to check blood pressure based on systolic and diastolic values.

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+function checkBP() {
+
+  let sys = Number(document.getElementById("sys").value);
+  let dia = Number(document.getElementById("dia").value);
+
+  let result = "";
+
+  if (sys >= 140 || dia >= 90) {
+    result = "High Blood Pressure";
+  } else if (sys < 90) {
+    result = "Low Blood Pressure";
+  } else {
+    result = "Normal Blood Pressure";
+  }
+
+  document.getElementById("result").innerText = result;
+}
+
 function calculateBMI() {
 
   let weight = Number(document.getElementById("weight").value);
